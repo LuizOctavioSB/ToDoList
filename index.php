f6ba39e5184abd021459282a319ea1cbd60cba0c
LuizOctavioSB <luizoctavios14@gmail.com>
2024-11-29 17:24:00 -0300

    adicionado segundo botao de incluir registro

--- index.php
+++ index.php
@@ -3,24 +3,26 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Lista de Tarefas</title>
+    <link rel="icon" href="assets/icons/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
 <body>
     <header>
-        <h1>Sistema Lista de Tarefas</h1>
+        <h1><i class="fa-solid fa-list-check"></i> Sistema Lista de Tarefas</h1>
     </header>
     <main>
         <div id="message"></div>
 
         <!-- Formulário para adicionar novas tarefas -->
-        <div class="task-input">
+        <div class="task-input" id="task-input-form">
             <input type="text" id="new-task" placeholder="Adicionar nova tarefa">
             <input type="date" id="task-date" placeholder="Data Limite">
             <input type="text" id="task-cost" placeholder="Custo (R$)">
-            <button id="add-task">Adicionar Tarefa</button>
+            <button id="add-task">Incluir Registro</button>
         </div>
 
         <!-- Lista de Tarefas Pendentes -->
@@ -29,6 +31,11 @@
         <!-- Botão para mostrar/ocultar tarefas concluídas -->
         <div class="toggle-completed">
             <button id="toggle-completed">Mostrar Tarefas Concluídas</button>
+        </div>
+
+        <!-- Botão para incluir novo registro -->
+        <div id="include-record" class="include-record">
+            <button id="include-record-button">Incluir Registro</button>
         </div>
 
         <!-- Lista de Tarefas Concluídas -->
